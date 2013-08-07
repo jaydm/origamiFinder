b097b1e888cbaf1d042c19c8d94fd092242695cd
Jay D. McHugh <jaydmchugh@gmail.com>
2013-08-07 12:41:42 -0500

    Created a new ModelTable class to handle a virtual database of models.
    Changed the QueryModelList class to remove the (non-functional) query
    against a server for model data.
    
    Still need to implement the actual searching.

---
+++ src/net/jnwd/origamiData/ModelTable.java
@@ -0,0 +1,140 @@
+package net.jnwd.origamiData;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import net.jnwd.origamiFinder.R;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.res.Resources;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class ModelTable {
+	private static final String TAG = "ModelDatabase";
+
+	public static final String COL_MODEL_NAME = "MODELNAME";
+	public static final String COL_MODEL_TYPE = "MODELTYPE";
+	public static final String COL_CREATOR = "CREATOR";
+	public static final String COL_BOOK_TITLE = "BOOKTITLE";
+	public static final String COL_ISBN = "ISBN";
+	public static final String COL_ON_PAGE = "ONPAGE";
+	public static final String COL_DIFFICULTY = "DIFFICULTY";
+	public static final String COL_PAPER = "PAPER";
+	public static final String COL_PIECES = "PIECES";
+	public static final String COL_GLUE = "GLUE";
+	public static final String COL_CUTS = "CUTS";
+
+	private static final String DATABASE_NAME = "MODELS";
+	private static final String FTS_VIRTUAL_TABLE = "FTS";
+	private static final int DATABASE_VERSION = 1;
+
+	private final DatabaseOpenHelper mDatabaseOpenHelper;
+
+	public ModelTable(Context context) {
+		mDatabaseOpenHelper = new DatabaseOpenHelper(context);
+	}
+
+	private static class DatabaseOpenHelper extends SQLiteOpenHelper {
+		private final Context mHelperContext;
+		private SQLiteDatabase mDatabase;
+
+		private static final String FTS_TABLE_CREATE = "" +
+			"CREATE VIRTUAL TABLE " + FTS_VIRTUAL_TABLE + " " +
+			"USING fts3 (" +
+			COL_MODEL_NAME + ", " +
+			COL_MODEL_TYPE + ", " +
+			COL_CREATOR + ", " +
+			COL_BOOK_TITLE + ", " +
+			COL_ISBN + ", " +
+			COL_ON_PAGE + ", " +
+			COL_DIFFICULTY + ", " +
+			COL_PAPER + ", " +
+			COL_PIECES + ", " +
+			COL_GLUE + ", " +
+			COL_CUTS + ")";
+
+		DatabaseOpenHelper(Context context) {
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+			mHelperContext = context;
+		}
+
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			mDatabase = db;
+
+			mDatabase.execSQL(FTS_TABLE_CREATE);
+
+			loadDatabase();
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			Log.w(TAG, "Upgrading database from version " + oldVersion + " to " + newVersion + ", which will destroy all old data");
+
+			db.execSQL("DROP TABLE IF EXISTS " + FTS_VIRTUAL_TABLE);
+
+			onCreate(db);
+		}
+
+		private void loadDatabase() {
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						loadModels();
+					} catch (IOException e) {
+						throw new RuntimeException(e);
+					}
+				}
+			}).start();
+		}
+
+		private void loadModels() throws IOException {
+			final Resources resources = mHelperContext.getResources();
+
+			InputStream inputStream = resources.openRawResource(R.raw.model);
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+			try {
+				String line;
+				Model model;
+
+				while ((line = reader.readLine()) != null) {
+					model = new Model(line);
+
+					long id = addModel(model);
+
+					if (id < 0) {
+						Log.e(TAG, "unable to add model: " + model.toString());
+					}
+				}
+			} finally {
+				reader.close();
+			}
+		}
+
+		public long addModel(Model model) {
+			ContentValues initialValues = new ContentValues();
+
+			initialValues.put(COL_MODEL_NAME, model.name);
+			initialValues.put(COL_MODEL_TYPE, model.modelType);
+			initialValues.put(COL_CREATOR, model.creator);
+			initialValues.put(COL_BOOK_TITLE, model.bookTitle);
+			initialValues.put(COL_ISBN, model.ISBN);
+			initialValues.put(COL_ON_PAGE, model.page);
+			initialValues.put(COL_DIFFICULTY, model.difficulty);
+			initialValues.put(COL_PAPER, model.paper);
+			initialValues.put(COL_PIECES, model.pieces);
+			initialValues.put(COL_GLUE, model.glue);
+			initialValues.put(COL_CUTS, model.cuts);
+
+			return mDatabase.insert(FTS_VIRTUAL_TABLE, null, initialValues);
+		}
+	}
+}
--- src/net/jnwd/origamiFinder/QueryModelList.java
+++ src/net/jnwd/origamiFinder/QueryModelList.java
@@ -1,22 +1,18 @@
 package net.jnwd.origamiFinder;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
+import net.jnwd.origamiData.Model;
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
 
 public class QueryModelList extends Activity implements OnClickListener {
+	List<Model> allModels = new ArrayList<Model>();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -52,55 +48,6 @@ public class QueryModelList extends Activity implements OnClickListener {
 	}
 
 	private void findModels() {
-		try {
-			StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 
-			StrictMode.setThreadPolicy(policy);
-
-			EditText modelName = (EditText) findViewById(R.id.txtMQueryName);
-			EditText bookTitle = (EditText) findViewById(R.id.txtMQueryTitle);
-			EditText creatorName = (EditText) findViewById(R.id.txtMQueryCreator);
-
-			String urlString = "" +
-				"http://puppet.pubint.com:8080/origami/findModel?" +
-				"modelName=" + modelName.getText() + "&" +
-				"bookTitle=" + bookTitle.getText() + "&" +
-				"creatorName=" + creatorName.getText();
-
-			URL url = new URL(urlString);
-
-			HttpURLConnection con = (HttpURLConnection) url.openConnection();
-
-			String data = readStream(con.getInputStream());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private String readStream(InputStream in) {
-		BufferedReader reader = null;
-
-		StringBuilder message = new StringBuilder();
-		String line;
-
-		try {
-			reader = new BufferedReader(new InputStreamReader(in));
-
-			while ((line = reader.readLine()) != null) {
-				message.append(line);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (reader != null) {
-				try {
-					reader.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-
-		return message.toString();
 	}
 }
